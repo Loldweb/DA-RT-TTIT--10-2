151af2b9aa00fdc2f7fbb3e612929917966cd0bb
Вторушин Денис Владимирович <gr621_vtdvl@ttit.local>
2022-10-31 12:38:03 +0700

    «DA-RT-TTIT--10-2»

---
+++ dart-ttit-10-2/bin/1-5.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  String txt = stdin.readLineSync()!;
+  List<String> words = txt.split(" ");
+  print("Start    Кол-во слов - ${words.length}      End");
+}
--- dart-ttit-10-2/bin/dart_ttit_10_2.dart
+++ dart-ttit-10-2/bin/dart_ttit_10_2.dart
@@ -3,3 +3,4 @@ import 'package:dart_ttit_10_2/dart_ttit_10_2.dart' as dart_ttit_10_2;
 void main(List<String> arguments) {
   print('Hello world: ${dart_ttit_10_2.calculate()}!');
 }
+ 
