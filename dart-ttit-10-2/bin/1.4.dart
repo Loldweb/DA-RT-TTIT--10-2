ab216fb52fe75d1c8c889fc215d37999f643535a
Вторушин Денис Владимирович <gr621_vtdvl@ttit.local>
2022-11-01 10:17:25 +0700

    «DA-RT-TTIT--10-2»

---
+++ dart-ttit-10-2/bin/1.4.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  var random = new Random();
+  List<int> num = [];
+  print("Введите до - ");
+  int max = int.parse(stdin.readLineSync()!);
+  print("Введте от - ");
+  int min = int.parse(stdin.readLineSync()!);
+  print(" ");
+  for (int i = 0; i < 10; ++i) {
+    stdout.write((min + Random().nextInt(max - min)));
+    stdout.write(" ");
+  }
+  /* print(num);
+  for (int i = 0; i < 10; ++i) {
+    print((min + Random().nextInt(max - min)));
+  }*/
+}
